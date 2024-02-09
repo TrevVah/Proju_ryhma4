060459666bf3354ad23ba3e7b3a6af38c6bbbf28
OlVai <olli20005@student.hamk.fi>
2024-02-09 17:43:55 +0200

    Add project files.

---
+++ Harkka/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KilsatMassiks
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<TabItem> _tabs;
+        public ObservableCollection<TabItem> Tabs
+        {
+            get { return _tabs; }
+            set
+            {
+                _tabs = value;
+                OnPropertyChanged("Tabs");
+            }
+        }
+
+        private TabItem _selectedTab;
+        public TabItem SelectedTab
+        {
+            get { return _selectedTab; }
+            set
+            {
+                _selectedTab = value;
+                OnPropertyChanged("SelectedTab");
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Tabs = new ObservableCollection<TabItem>();
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void AddTab_Click(object sender, RoutedEventArgs e)
+        {
+            var newTab = new TabItem();
+            newTab.Header = $"Tab {Tabs.Count + 1}";
+            newTab.Content = new TabContent();
+            Tabs.Add(newTab);
+        }
+
+    }
+}
