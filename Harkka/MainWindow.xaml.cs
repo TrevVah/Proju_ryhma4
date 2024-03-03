73ae26698afe4372c0098c3a67a7d577f8845ecd
OlVai <olli20005@student.hamk.fi>
2024-03-03 17:20:03 +0200

    Tabien nimet oikein "tab #" sijaan. Raportti sivun ohjauspaneelille lisätty ominaisuuksia, jotta pysyvät järkevästi paikoillaan ikkunan koon muokatessa.

--- Harkka/MainWindow.xaml.cs
+++ Harkka/MainWindow.xaml.cs
@@ -58,7 +58,7 @@ namespace KilsatMassiks
 
         private void AddTab_Click(object sender, RoutedEventArgs e)
         {
-            var newTab = CreateNewTabItem($"Tab {Tabs.Count + 1}", new TabContent());
+            var newTab = CreateNewTabItem($"Tyhjä", new TabContent());
             Tabs.Add(newTab);
             SelectedTab = newTab;
         }
@@ -75,7 +75,7 @@ namespace KilsatMassiks
 
         private void OpenCalendarTab(object sender, RoutedEventArgs e)
         {
-            var newTab = CreateNewTabItem($"Tab {Tabs.Count + 1}", new calendar());
+            var newTab = CreateNewTabItem($"Matkan lisäys", new calendar());
             Tabs.Add(newTab);
             SelectedTab = newTab;
 
@@ -89,7 +89,7 @@ namespace KilsatMassiks
 
         private void OpenReportTab(object sender, RoutedEventArgs e)
         {
-            var newTab = CreateNewTabItem($"Tab {Tabs.Count + 1}", new Raportti());
+            var newTab = CreateNewTabItem($"Raportin luonti", new Raportti());
             Tabs.Add(newTab);
             SelectedTab = newTab;
         }
